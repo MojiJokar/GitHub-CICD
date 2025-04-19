d20878fd32e532143c65ceb230d0a533e250a98a
Moji <94471127+MojiJokar@users.noreply.github.com>
2025-04-19 12:00:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ import os
 def main():
     # Access secrets and workflow inputs from environment variables
     api_key = os.environ.get("API_KEY")
-    user_input = os.environ.get("INPUT_USER_INPUT")
+    #user_input = os.environ.get("INPUT_USER_INPUT")
 
     print(f"API_KEY: {api_key}")
-    print(f"Workflow Input: {user_input}")
+    #print(f"Workflow Input: {user_input}")
 
 if __name__ == "__main__":
     main()
